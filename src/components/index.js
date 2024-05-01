e7d3f4d6bef613fbe1a7f8542d8231cc0854f342
saumitra.shukla@pentair.com <saumitra.shukla@pentair.com>
2024-05-01 10:57:31 +0530

    Inital Setup

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// export { default as Article } from "./article/Article";
+// export { default as Brand } from "./brand/Brand";
+// export { default as Cta } from "./cta/Cta";
+// export { default as Feature } from "./feature/Feature";
+// export { default as Navbar } from "./navbar/Navbar";
+import Article from "./article/Article";
+import Brand from "./brand/Brand";
+import Cta from "./cta/Cta";
+import Feature from "./feature/Feature";
+import Navbar from "./navbar/Navbar";
+
+export { Article, Brand, Cta, Feature, Navbar };
